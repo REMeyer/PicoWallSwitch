5e0313fdc2b46e67f975492b97b60da17ebdad35
REMeyer <remphys@gmail.com>
2021-11-17 11:59:39 +0000

    Updating readme

--- README.md
+++ README.md
@@ -5,10 +5,10 @@ Micropython software that provides control of a 3001HB Servo and a Pimoroni RV30
 The module contains three scripts:
 
 * rv3028_pico.py
-** Provides functionality for the RV3028 RTC breakout module
+	* Provides functionality for the RV3028 RTC breakout module
 * servo_3001HB_pico.py
-** Provides very basic functionality for the 3001HB Servo
+	* Provides very basic functionality for the 3001HB Servo
 * heat_control.py
-** Mainloop script for controlling the wall switch - specialized application
+	* Mainloop script for controlling the wall switch - specialized application
 
 This library has no specific dependancies other than micropython-included modules.
--- servo_3001HB_pico.py
+++ servo_3001HB_pico.py
@@ -1,9 +1,9 @@
 from machine import Pin, PWM
 import utime
 
-# Duty cycle min: 4%, neutral: 7.5%, max: 11%
+# From specsheet - duty cycle min: 4%, neutral: 7.5%, max: 11%
 # So min 2650, neutral 4915, max: 7200
-# From testing: 8100 max, 1800 min, 4950
+# From testing - max: 8100, min: 1800, neutral: 4950
 
 SERVO_3001HB_PIN = 12
 FRONT = 0.75
@@ -13,34 +13,33 @@ BACK = 0.2
 class SERVO3001HB():
 
     def __init__(self):
-
+        #Initializes and moves the servo to the neutral position
         self.servo = PWM(Pin(SERVO_3001HB_PIN))
         self.servo.freq(50)
         self.servo_move(NEUTRAL)
 
-    def servo_move(self, p):
+    def move(self, p):
         '''
         Takes a percentage (p) and moves the servo
         to the range represented by p.
         '''
-        
         self.servo.duty_u16(int(1800. + p*(8100. - 1800.)))
 
     def switch_on(self):
         #Move to turn on switch
-        self.servo_move(FRONT)
+        self.move(FRONT)
         utime.sleep(0.5)
         #Move back to neutral
         self.servo_move(NEUTRAL)
 
     def switch_off(self):
         #Move to turn off switch
-        self.servo_move(BACK)
+        self.move(BACK)
         utime.sleep(0.5)
         #Move back to neutral
         self.servo_move(NEUTRAL)
 
     def move_neutral(self):
         #Move to neutral
-        self.servo_move(NEUTRAL)
+        self.move(NEUTRAL)
 
